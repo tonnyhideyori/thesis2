690826d21df1241967838350657b7e3186b694e5
fariha7758 <hossain7758@gmail.com>
2022-03-21 00:10:21 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,23 +1,47 @@
+import csv
 from new_alpha import AI
 from size6 import size6
-ai =AI("player1",5)
-size=size6(6)
-size.board()
-player="player1"
-while sum(size.game[0:6]) != 0 and sum(size.game[7:13]) != 0:
-    if ai.player == player:
-        print("AI",player)
-        move=ai.move_series(size)
-        temps=size.game[move]
-        size.game[move]=0
-        result=size.movingforward(temps,move,"player1")
-        if result[0]==1 and (result[2]!=6 and result[2]!=13):
-            size.takeover(result[2],player)
+from time import time
+import pickle
+total_time=0
+
+for x in range(2):
+    ai =AI("player1",5)
+    size=size6(6)
+    size.board()
+    player="player1"
+
+
+    while sum(size.game[0:6]) != 0 and sum(size.game[7:13]) != 0:
+        t = time()
+
+
+
+        if ai.player == player:
+            print("AI",player)
+            move=ai.move_series(size)
+            temps=size.game[move]
+            size.game[move]=0
+            result=size.movingforward(temps,move,"player1")
+            print("Calculated in %.1fs" % (time() - t))
+            total_time += time() - t
+            if result[0]==1 and (result[2]!=6 and result[2]!=13):
+                size.takeover(result[2],player)
+            else:
+                player="player2"
+            size.board()
         else:
-            player="player2"
-        size.board()
-    else:
-        player=size.play("player2")
-        player = "player1"
-size.winner()
+            player=size.play("player2")
+            player = "player1"
+    print("total time is %.1fs" % (total_time))
+    size.winner()
+
+    with open('time.csv', 'w', newline='') as csvfile:
+        fieldnames = ['winner', 'total_time']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow({'winner': size.winner, 'total_time': total_time})
+        csvfile.close()
+
     
